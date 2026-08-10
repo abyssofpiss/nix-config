b3fa1ec02b976fcb2a3a6de5769d1da52b7dc3f8
abyss <abyssofpiss@gmail.com>
2026-08-10 23:21:59 +0800

    noctalia

--- modules/niri-desktop.nix
+++ modules/niri-desktop.nix
@@ -8,4 +8,17 @@
     pkgs.alacritty 
     pkgs.brightnessctl
   ];
+
+  systemd.user.tmpfiles.rules = [
+    "d /home/abyss/.config/niri 0755 abyss users - -"
+    "L+ /home/abyss/.config/niri/config.kdl - - - - ${pkgs.writeText "config.kdl" ''
+      input { keyboard { xkb { layout "us"; }; }; }
+      binds {
+          "Super+T" { spawn "alacritty"; }
+          "Super+D" { spawn "noctalia"; }
+          "Super+Q" { close-window; }
+      }
+      spawn-at-startup "noctalia"
+    ''}"
+  ];
 }
