a54e44a916b81c74dc4d6d1f170d9b1d90d85e5f
abyss <abyssofpiss@gmail.com>
2026-08-11 11:49:40 +0800

    something broke so i fix

--- modules/niri-desktop.nix
+++ modules/niri-desktop.nix
@@ -5,20 +5,6 @@
 
   environment.systemPackages = [
     inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
-    pkgs.alacritty 
     pkgs.brightnessctl
-  ];
-
-  systemd.user.tmpfiles.rules = [
-    "d /home/abyss/.config/niri 0755 abyss users - -"
-    "L+ /home/abyss/.config/niri/config.kdl - - - - ${pkgs.writeText "config.kdl" ''
-      input { keyboard { xkb { layout "us"; }; }; }
-      binds {
-          "Super+T" { spawn "alacritty"; }
-          "Super+D" { spawn "noctalia"; }
-          "Super+Q" { close-window; }
-      }
-      spawn-at-startup "noctalia"
-    ''}"
   ];
 }
