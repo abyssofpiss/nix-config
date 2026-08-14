04470da2da293d6585095ef02225a7b4b2332e72
abyss <abyssofpiss@gmail.com>
2026-08-14 19:16:05 +0800

    edited

--- modules/niri-desktop.nix
+++ modules/niri-desktop.nix
@@ -7,7 +7,15 @@
     pkgs.brightnessctl
     pkgs.kitty
     pkgs.fuzzel
+    pkgs.qt6.qtwayland
+    pkgs.qt6.qtbase
+    pkgs.libxcb-cursor
   ];
+
+  environment.sessionVariables = {
+    QT_QPA_PLATFORM = "wayland";
+    QT_PLUGIN_PATH = "${pkgs.qt6.qtwayland}/lib/qt-6/plugins:${pkgs.qt6.qtbase}/lib/qt-6/plugins";
+  };
 
   programs.niri = {
     enable = true;
