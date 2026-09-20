a4dee74a1da9d5bcab4713b73aec287058cf2170
abyss <abyssofpiss@gmail.com>
2026-09-20 14:13:37 +0800

    apps

--- modules/apps.nix
+++ modules/apps.nix
@@ -75,5 +75,12 @@
 
     #Game
     osu-lazer
+
+    #Stuff for quickshell wallpapers
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
   ];
 }
