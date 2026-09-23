40b017d192bc4480f059fc5616a042bda1d13684
abyss <abyssofpiss@gmail.com>
2026-09-23 21:59:58 +0800

    starship theme

--- modules/apps.nix
+++ modules/apps.nix
@@ -51,6 +51,74 @@
   # Starship
   programs.starship = {
     enable = true;
+    settings = {
+      format = "[░▒▓](tokyo_blue)[  ](bg:tokyo_blue fg:tokyo_black)[](fg:tokyo_blue bg:tokyo_subtle)[ $directory ](fg:tokyo_white bg:tokyo_subtle)[](fg:tokyo_subtle bg:tokyo_subtle2)[ $git_branch$git_status ](fg:tokyo_blue bg:tokyo_subtle2)[](fg:tokyo_subtle2 bg:tokyo_base)[ $nodejs$rust$golang$php ](fg:tokyo_blue bg:tokyo_base)[](fg:tokyo_base bg:tokyo_dark)[ $time ](fg:tokyo_subtle bg:tokyo_dark)[ ](fg:tokyo_dark)\n$character";
+      palette = "tokyo_night";
+      palettes.tokyo_night = {
+        tokyo_black = "#15161e";
+        tokyo_blue = "#7aa2f7";
+        tokyo_white = "#c0caf5";
+        tokyo_subtle = "#292e42";
+        tokyo_subtle2 = "#3b4261";
+        tokyo_base = "#1a1b26";
+        tokyo_dark = "#16161e";
+      };
+
+      directory = {
+        style = "fg:tokyo_white bg:tokyo_subtle";
+        format = "[$path]($style)";
+        truncation_length = 3;
+        truncation_symbol = "…/";
+      };
+     
+      git_branch = {
+        symbol = "";
+        style = "bg:tokyo_subtle2";
+        format = "[[ $symbol $branch ](fg:tokyo_blue bg:tokyo_subtle2)]($style)";
+      };
+
+      git_status = {
+        style = "bg:tokyo_subtle2";
+        format = "[[($all_status$ahead_behind )](fg:tokyo_blue bg:tokyo_subtle2)]($style)";
+      };
+
+      nodejs = {
+        symbol = "";
+        style = "bg:tokyo_base";
+        format = "[[ $symbol ($version) ](fg:tokyo_blue bg:tokyo_base)]($style)";
+      };
+
+      rust = {
+        symbol = "";
+        style = "bg:tokyo_base";
+        format = "[[ $symbol ($version) ](fg:tokyo_blue bg:tokyo_base)]($style)";
+      };
+
+      golang = {
+        symbol = "";
+        style = "bg:tokyo_base";
+        format = "[[ $symbol ($version) ](fg:tokyo_blue bg:tokyo_base)]($style)";
+      };
+
+      php = {
+        symbol = "🐘";
+        style = "bg:tokyo_base";
+        format = "[[ $symbol ($version) ](fg:tokyo_blue bg:tokyo_base)]($style)";
+      };
+
+      time = {
+        disabled = false;
+        time_format = "%R"; # Hour:Minute Format
+        style = "bg:tokyo_dark";
+        format = "[[  $time ](fg:tokyo_white bg:tokyo_dark)]($style)";
+      };
+
+      character = {
+        disabled = false;
+        success_symbol = "[❯](bold green)";
+        error_symbol = "[❯](bold red)";
+      };
+    };
   };
 
   # Daily Drivers and Terminal Utilities
